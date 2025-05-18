9a55796a7baab13e9b68a1ccc66655025640c0be
SRISABITHA06 <srisabithasabitha56@gmail.com>
2025-05-18 21:04:36 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,15 @@
+provider "google" {
+  project = var.project_id
+  region  = var.region
+}
+
+resource "google_container_cluster" "primary" {
+  name     = "prod-cluster"
+  location = var.region
+
+  initial_node_count = 3
+
+  node_config {
+    machine_type = "e2-medium"
+  }
+}
